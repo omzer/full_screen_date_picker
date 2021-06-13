daee0f192631ddb1a1605b6073fb3c229bb2046b
Omar Sabri <o.sabri@freightos.com>
2021-06-13 15:18:53 +0300

    Add tests

--- test/full_screen_date_picker_test.dart
+++ test/full_screen_date_picker_test.dart
@@ -1,3 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:full_screen_date_picker/date_logic.dart';
 
-void main() => test('adds one to input values', () {});
+void main() {
+  test('Test getting previous sunday', () {
+    DateTime result = DatePickingLogic.previousSunday(DateTime.now());
+    expect(result.weekday, DateTime.sunday);
+  });
+
+  test('Test getting first day of a month', () {
+    DateTime result = DatePickingLogic.firstDayOfTheMonth(0);
+    expect(result.day, 1);
+  });
+
+  test('Test getting first day of a month', () {
+    DateTime now = DateTime.now();
+    DateTime yesterday = now.subtract(Duration(days: 1));
+    bool result = DatePickingLogic.isBeforeNow(yesterday);
+    expect(result, yesterday.isBefore(now));
+  });
+
+}
